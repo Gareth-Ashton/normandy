bd7164b4ccb43b1ecb224443493626a27f85854f
Gareth-Ashton <garethlovesarchitects@gmail.com>
2024-04-11 13:55:03 +0200

    Updated Date Area

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const countdown = () => {
-    const countDate = new Date('May 1, 2024 00:00:00').getTime();
+    const countDate = new Date('April 29, 2024 00:00:00').getTime();
     const now = new Date().getTime();
     const gap = countDate - now;
   
